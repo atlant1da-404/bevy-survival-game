fa77b3cc9c846c3ee79f26b3a4a4eaa706db0fc6
AndriiK-2003 <andriik@lcgroups.com>
2022-05-05 19:58:35 +0300

    Add window presentation for game

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+use bevy::prelude::*;
+
+
+const WIDTH: f32 = 900.;
+const HEIGHT: f32 = 600.;
+const NAME: &str = "bevy-survival-game";
+
+
+
 fn main() {
-    println!("Hello, world!");
+
+    App::new()
+        .insert_resource(WindowDescriptor {
+            width: WIDTH,
+            height: HEIGHT,
+            title: NAME.to_string(),
+            ..Default::default()
+        })
+        .add_plugins(DefaultPlugins).run();
+
 }
