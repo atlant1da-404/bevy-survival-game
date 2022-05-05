79598574a598b93bcca7b22f1d318d989c40e046
AndriiK-2003 <andriik@lcgroups.com>
2022-05-05 20:28:33 +0300

    Add graphics module and load assets

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+mod graphics;
+
 use bevy::prelude::*;
 
 
@@ -6,16 +8,18 @@ const HEIGHT: f32 = 600.;
 const NAME: &str = "bevy-survival-game";
 
 
-
 fn main() {
 
     App::new()
+        .insert_resource(ClearColor(Color::rgb(0.3, 0.5, 0.3)))
         .insert_resource(WindowDescriptor {
             width: WIDTH,
             height: HEIGHT,
             title: NAME.to_string(),
             ..Default::default()
         })
+        .add_startup_system_to_stage(StartupStage::PreStartup, graphics::load_graphics)
+        .add_startup_system(graphics::spawn_camera)
         .add_plugins(DefaultPlugins).run();
 
 }
